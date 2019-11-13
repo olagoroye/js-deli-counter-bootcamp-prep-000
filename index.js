3544abb5d239ad3612e824fe6fcfdac6dad5dd8f
olusola olagoroye <solaolagoroye@gmail.com>
2019-11-13 17:29:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,15 @@ function nowServing(katzDeliLine){
 
 function currentLine(Line){
   var outputString;
-
+if (katzDeliLine.length > 0) {
+      for(var i = 0; i < katzDeliLine.length; i++) {
+        output += (i + 1) + ". " + katzDeliLine[i] + ", ";
+      }
+      output = output.slice(0, output.length - 2);
+      return "The line is currently: " + output;
+    } else {
+        return "The line is currently empty.";
+    }
 }
 
   
